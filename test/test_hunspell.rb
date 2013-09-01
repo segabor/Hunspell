eb75be5a27316e464c93ce4ccc127feed08ee6ca
Gábor Sebestyén <segabor@gmail.com>
2013-09-01 10:21:52 +0200

    Add tests

---
+++ test/test_hunspell.rb
@@ -0,0 +1,25 @@
+require 'test/unit'
+require "Hunspell"
+
+class HunspellTest < Test::Unit::TestCase
+  def setup
+    @sp = Hunspell.new("test/dict/hu_HU.aff", "test/dict/hu_HU.dic") 
+  end
+
+  def test_spellcheck
+    assert @sp.spellcheck('ablak')
+  end
+
+  def test_false_spellcheck
+    assert !@sp.spellcheck("paprica")
+  end
+
+  def test_suggestions
+    assert !@sp.spellcheck('paprica')
+
+    suggestions = @sp.suggest("paprica")
+    assert !suggestions.include?('paprica')
+    assert suggestions.include?('paprika')
+  end
+end
+
